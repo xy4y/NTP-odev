547bad6a220102ccc660c984c95863041e34c6e1
bashar <22010903026@subu.edu.tr>
2025-01-04 16:33:07 +0300

    oyun kodu yorumda acııklandım

--- NTP-odev/cpp/Oyun.cpp
+++ NTP-odev/cpp/Oyun.cpp
@@ -1,68 +1,77 @@
-#include "Oyun.h"
-#include <iostream>
+#include "Oyun.h" // Oyun sınıfını ekliyoruz
+#include <iostream> // Giriş/çıkış kütüphanesini ekliyoruz
 
+// Oyun sınıfının kurucusu
 Oyun::Oyun() : polis(), hirsiz() {}
 
+// Haritayı güncelleyen fonksiyon (polis ve hırsızın yerini belirler)
 void Oyun::haritaGuncelle()
 {
+    // Haritayı temizler (tüm hücrelere '.' koyar)
     for (int i = 0; i < 10; ++i)
         for (int j = 0; j < 10; ++j)
             harita[i][j] = '.';
 
+    // Polisin ve hırsızın yerini haritada günceller
     harita[polis.getX()][polis.getY()] = 'P';
     harita[hirsiz.getX()][hirsiz.getY()] = 'H';
 }
 
+// Haritayı ekrana gösteren fonksiyon
 void Oyun::haritaGoster()
 {
     std::cout << "\nHarita:" << std::endl;
     for (int i = 0; i < 10; ++i)
     {
         for (int j = 0; j < 10; ++j)
-            std::cout << harita[i][j] << ' ';
-        std::cout << std::endl;
+            std::cout << harita[i][j] << ' '; // Satırdaki her hücreyi yazdırır
+        std::cout << std::endl; // Sonraki satıra geçer
     }
 }
 
+// Oyunu çalıştıran fonksiyon
 void Oyun::oynat()
 {
-    char hareket;
-    bool oyunBitti = false;
+    char hareket; // Oyuncunun seçimini saklar
+    bool oyunBitti = false; // Oyunun bitip bitmediğini kontrol eder
 
-    while (!oyunBitti)
+    while (!oyunBitti) // Oyun bitene kadar devam eder
     {
-        haritaGuncelle();
-        haritaGoster();
+        haritaGuncelle(); // Haritayı günceller
+        haritaGoster(); // Haritayı ekrana gösterir
 
+        // Polis için seçenek sorar (kovala veya dinlen)
         std::cout << "\nPolis ne yapmak ister? (k - Kos / d - Dinlen): ";
         std::cin >> hareket;
 
-        if (hareket == 'k')
+        if (hareket == 'k') // Eğer polis "kovala" derse
             polis.kovala();
-        else if (hareket == 'd')
+        else if (hareket == 'd') // Eğer polis "dinlen" derse
             polis.dinlen();
 
+        // Hırsız için seçenek sorar (kaç veya saklan)
         std::cout << "\nHirsiz ne yapmak ister? (k - Kac / s - Saklan): ";
         std::cin >> hareket;
 
-        if (hareket == 'k')
+        if (hareket == 'k') // Eğer hırsız "kaç" derse
             hirsiz.kac();
-        else if (hareket == 's')
+        else if (hareket == 's') // Eğer hırsız "saklan" derse
             hirsiz.saklan();
 
+        // Oyunun bitme durumunu kontrol eder
         if (polis.getDayaniklilik() <= 0 && hirsiz.getDayaniklilik() <= 0)
         {
-            std::cout << "ikisi yorulmus! Oyun bitti.\n";
+            std::cout << "Ikisi yorulmus! Oyun bitti.\n"; // İkisi de yorulduysa
             oyunBitti = true;
         }
         else if (polis.getDayaniklilik() <= 0)
         {
-            std::cout << "Polis yorulmus! Hirsiz kazandi.\n";
+            std::cout << "Polis yorulmus! Hirsiz kazandi.\n"; // Polis yorulduysa
             oyunBitti = true;
         }
         else if (hirsiz.getDayaniklilik() <= 0)
         {
-            std::cout << "Hirsiz yorulmus! Polis kazandi.\n";
+            std::cout << "Hirsiz yorulmus! Polis kazandi.\n"; // Hırsız yorulduysa
             oyunBitti = true;
         }
     }
